ba7d7ab28deee24de45ae3914bf187a31fc9bf03
Yeung Yiu Hung <hkclex@gmail.com>
2017-09-01 19:48:26 +0800

    Update basic folder structure

---
+++ BugBlaster/BugBlaster.h
@@ -0,0 +1,19 @@
+//
+//  BugBlaster.h
+//  BugBlaster
+//
+//  Created by Yeung Yiu Hung on 1/9/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BugBlaster.
+FOUNDATION_EXPORT double BugBlasterVersionNumber;
+
+//! Project version string for BugBlaster.
+FOUNDATION_EXPORT const unsigned char BugBlasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BugBlaster/PublicHeader.h>
+
+
